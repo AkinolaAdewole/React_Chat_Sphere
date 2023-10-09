f0680d7b89e9664de50020eec70c13e7c9037817
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-09 13:07:46 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { ChatEngine } from 'react-chat-engine';
+import './App.css';
 
 function App() {
   return (
     <>
+     <ChatEngine
+      height="100vh"
+      projectID= "11a880eb-2922-49ae-9def-260e328ee01a"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
    </>
   );
 }
