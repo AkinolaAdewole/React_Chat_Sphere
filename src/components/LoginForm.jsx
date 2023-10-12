1a4ba1ce57097467bd5fc261aee80b7fe4a129c0
Akinola Adewole <adewoleakinola24@gmail.com>
2023-10-12 12:01:38 +0100

    commit

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,49 +1,60 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID= "11a880eb-2922-49ae-9def-260e328ee01a"
+// Define a constant variable for the Chat Engine project ID (You should replace it with your actual project ID)
+const projectID = "11a880eb-2922-49ae-9def-260e328ee01a";
 
 const Modal = () => {
+  // Define and initialize state variables for username, password, and error messages
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
+  // Define a function to handle the form submission
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevent the default form submission behavior (page refresh)
 
+    // Create an authentication object with Project-ID, User-Name, and User-Secret
     const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
 
     try {
+      // Attempt to make an HTTP GET request to retrieve chats with provided authentication headers
       await axios.get('https://api.chatengine.io/chats', { headers: authObject });
 
+      // If the request is successful, store the username and password in localStorage
       localStorage.setItem('username', username);
       localStorage.setItem('password', password);
+      console.log(username); // Log the username to the console
 
-      window.location.reload();
-      setError('');
+      window.location.reload(); // Refresh the window
+      setError(''); // Clear any previous error messages
     } catch (err) {
-      setError('Oops, incorrect credentials.');
+      setError('Oops, incorrect credentials.'); // Set an error message in case of authentication failure
     }
   };
 
+  // Return the JSX (user interface) for the Modal component
   return (
     <div className="wrapper">
       <div className="form">
         <h1 className="title">Chat Application</h1>
         <form onSubmit={handleSubmit}>
+          {/* Input fields for username and password */}
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
+            {/* Submit button */}
             <button type="submit" className="button">
               <span>Start chatting</span>
             </button>
           </div>
         </form>
+        {/* Display error message, if any */}
         <h1>{error}</h1>
       </div>
     </div>
-
   );
 };
 
+// Export the Modal component for use in other parts of the application
 export default Modal;
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -2,40 +2,51 @@ import { useState } from 'react';
 import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 import { sendMessage, isTyping } from 'react-chat-engine';
 
+// Define a functional component named MessageForm
 const MessageForm = (props) => {
+  // Initialize a state variable 'value' and a function 'setValue' to update it using the 'useState' hook
   const [value, setValue] = useState('');
-  const { chatId, creds } = props;
+  const { chatId, creds } = props; // Destructuring props for ease of use
 
+  // Handle changes in the message input field
   const handleChange = (event) => {
+    // Update the 'value' state with the current value of the input field
     setValue(event.target.value);
 
+    // Notify the chat engine that the user is typing
     isTyping(props, chatId);
   };
 
+  // Handle form submission when the user clicks the send button
   const handleSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the default form submission behavior
 
-    const text = value.trim();
+    const text = value.trim(); // Trim leading and trailing whitespace from the message
 
+    // Check if the trimmed message is not empty
     if (text.length > 0) {
+      // Send the message using the 'sendMessage' function provided by the chat engine
       sendMessage(creds, chatId, { text });
     }
 
+    // Clear the message input field by resetting the 'value' state
     setValue('');
   };
 
+  // Handle file uploads
   const handleUpload = (event) => {
+    // Send the uploaded files as a message using the 'sendMessage' function, including any provided text
     sendMessage(creds, chatId, { files: event.target.files, text: '' });
   };
 
+  // Render the message input form
   return (
     <form className="message-form" onSubmit={handleSubmit}>
       <input
         className="message-input"
         placeholder="Send a message..."
-        value={value}
-        onChange={handleChange}
-        onSubmit={handleSubmit}
+        value={value} // Bind the input value to the 'value' state
+        onChange={handleChange} // Call the 'handleChange' function when the input changes
       />
       <label htmlFor="upload-button">
         <span className="image-button">
@@ -47,13 +58,14 @@ const MessageForm = (props) => {
         multiple={false}
         id="upload-button"
         style={{ display: 'none' }}
-        onChange={handleUpload.bind(this)}
+        onChange={handleUpload.bind(this)} // Call the 'handleUpload' function when a file is selected
       />
-      <button type="submit" className="send-button">
+      <button type="submit" className="send-button" onSubmit={handleSubmit}>
         <SendOutlined className="send-icon" />
       </button>
     </form>
   );
 };
 
+// Export the MessageForm component for use in other parts of the application
 export default MessageForm;
